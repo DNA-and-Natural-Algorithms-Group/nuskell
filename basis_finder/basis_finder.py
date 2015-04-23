b797847b89af339398f3ed1eb64f432cca5982e9
Seung Woo Shin <seungwoo@eecs.berkeley.edu>
2015-04-23 06:26:33 -0700

    More simple bug fixes.

--- basis_finder/basis_finder.py
+++ basis_finder/basis_finder.py
@@ -387,16 +387,16 @@ def find_basis(crn, fs, optimize = True):
             if x:
                 print "Found that "+x+" is equivalent to "+y+"."
                 b = enumerate_basis(c, list(fs)+[x])
-                if b == None: return None
-                b2 = []
-                for [r, p] in b:
-                    r1 = [z if z != x else y for z in r]
-                    p1 = [z if z != x else y for z in p]
-                    b2.append([r1,p1])
-                #b2 = enumerate_basis(b, fs)
-                #if b2 == None: return None
-                basis += b2
-                continue
+                if b != None:
+                    b2 = []
+                    for [r, p] in b:
+                        r1 = [z if z != x else y for z in r]
+                        p1 = [z if z != x else y for z in p]
+                        b2.append([r1,p1])
+                    #b2 = enumerate_basis(b, fs)
+                    #if b2 == None: return None
+                    basis += b2
+                    continue
             ##
             b = enumerate_basis(c, fs)
             if b == None: # irregular or nontidy
