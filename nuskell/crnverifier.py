1aa0f25b30b923efb02ccce685dc1b2999347854
Jacksonnnnn <jhuse01@gmail.com>
2023-06-05 16:46:01 -0400

    Updated crnverifier.py to allow compatibiltiy on Windows computers. Previously, SIGALRM was used but this is only available on Python running UNIX.

--- nuskell/crnverifier.py
+++ nuskell/crnverifier.py
@@ -18,11 +18,34 @@ from crnverifier import (pathway_decomposition_eq,
                          compositional_hybrid_test,
                          modular_crn_bisimulation_test)
 
-class TimeoutError(Exception):
-    pass
+from threading import Thread
+import functools
 
-def handler(signum, frame):
-    raise TimeoutError('Time over')
+def timer_func(timeout):
+    def deco(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            res = [Exception('function [%s] timeout [%s seconds] exceeded!' % (func.__name__, timeout))]
+            def newFunc():
+                try:
+                   res[0] = func(*args, **kwargs)
+                except Exception as e:
+                    res[0] = e
+
+            t = Thread(target=newFunc)
+            t.daemon = True
+            try:
+                t.start()
+                t.join(timeout)
+            except Exception as je:
+                print('error starting thread')
+                raise je
+            ret = res[0]
+            if isinstance(ret, BaseException):
+                raise ret
+            return ret
+        return wrapper
+    return deco
 
 def verify(fcrn, icrn, formals, method, interpretation = None, timeout = 0):
     """Verify the equivalence of a formal CRN and its implementation CRN.
@@ -53,8 +76,8 @@ def verify(fcrn, icrn, formals, method, interpretation = None, timeout = 0):
     fcrn = [list(rxn[:2]) for rxn in split_reversible_rxns(fcrn)]
     icrn = [list(rxn[:2]) for rxn in split_reversible_rxns(icrn)]
 
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(timeout)
+    timer_func(timeout)
+
     try:
         if 'crn-bisimulation' in method:
             if method[-2:] == '-ls':
@@ -63,39 +86,37 @@ def verify(fcrn, icrn, formals, method, interpretation = None, timeout = 0):
                 permissive_check = 'bruteforce'
             else:
                 permissive_check = 'graphsearch'
-            v, i = crn_bisimulation_test(fcrn, 
-                                         icrn, 
+            v, i = crn_bisimulation_test(fcrn,
+                                         icrn,
                                          formals,
                                          interpretation = interpretation,
-                                         permissive = permissive_check) 
+                                         permissive = permissive_check)
         elif method == 'pathway-decomposition':
             v = pathway_decomposition_eq([fcrn, icrn], formals)
             i = None
         elif method == 'compositional-hybrid':
-            v, i = compositional_hybrid_test(fcrn, 
-                                             icrn, 
+            v, i = compositional_hybrid_test(fcrn,
+                                             icrn,
                                              formals,
                                              interpretation)
         elif method == 'integrated-hybrid':
-            v, i = integrated_hybrid_test(fcrn, 
-                                          icrn, 
+            v, i = integrated_hybrid_test(fcrn,
+                                          icrn,
                                           formals,
                                           interpretation)
         else:
             raise RuntimeError('Unknown verification method.')
-    except TimeoutError:
+    except Exception:
         v, i = None, None
     finally:
-        signal.alarm(0)
-    return v, i
+        return v, i
 
 def verify_modules(fcrns, icrns, formals, method, interpretation = None, timeout = 0):
     """ Choose from different algorithms for modular CRN bisimulation. """
     fcrns = [[list(rxn[:2]) for rxn in split_reversible_rxns(mod)] for mod in fcrns]
     icrns = [[list(rxn[:2]) for rxn in split_reversible_rxns(mod)] for mod in icrns]
 
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(timeout)
+    timer_func(timeout)
     try:
         if 'crn-bisimulation' in method:
             if method[-2:] == '-ls':
@@ -104,15 +125,14 @@ def verify_modules(fcrns, icrns, formals, method, interpretation = None, timeout
                 permissive_check = 'bruteforce'
             else:
                 permissive_check = 'graphsearch'
-            v, i = modular_crn_bisimulation_test(fcrns, 
-                                                 icrns, 
+            v, i = modular_crn_bisimulation_test(fcrns,
+                                                 icrns,
                                                  formals,
-                                                 interpretation = interpretation, 
+                                                 interpretation = interpretation,
                                                  permissive = permissive_check)
         else:
             raise RuntimeError('Unsupported verification method.')
     except TimeoutError:
         v, i = None, None
     finally:
-        signal.alarm(0)
-    return v, i
+        return v, i
